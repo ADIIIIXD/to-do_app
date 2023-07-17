db010fddaed818eed12a0128af46027a15bf529d
aditya <addie4120@gmail.com>
2023-07-17 22:29:50 +0530

    functions added to todo.py file

--- todo.py
+++ todo.py
@@ -1,7 +1,4 @@
-def get_todo(filename):
-    with open(filename, "r") as file:
-        read_todos = file.readlines()
-    return read_todos
+import functions
 
 
 while True:
@@ -11,15 +8,14 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:] + "\n"
 
-        todos = get_todo("to-do.txt")
+        todos = functions.get_todo()
 
         todos.append(todo)
 
-        with open("to-do.txt", "w") as file:
-            file.writelines(todos)
+        functions.write_todo(todos)
 
     elif user_action.startswith("show"):
-        todos = get_todo("to-do.txt")
+        todos = functions.get_todo()
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -27,30 +23,28 @@ while True:
 
     elif user_action.startswith("edit"):
         try:
-            todos = get_todo("to-do.txt")
+            todos = functions.get_todo()
             print("Here is the existing list \n", [todo.strip("\n") for todo in todos])
             num = int(input("Enter the number of to-do task you want to edit : "))
             num = num - 1
             new_todo = input("What do you want it to replace it with : ")
             todos[num] = new_todo + "\n"
-            with open("to-do.txt", "w") as file:
-                todos = file.writelines(todos)
+            functions.write_todo(todos)
         except ValueError:
             print("Your input is not valid")
             continue
 
     elif user_action.startswith("completed"):
         try:
-            get_todo("to-do.txt")
+            todos = functions.get_todo()
             print(todos)
             num = int(user_action[9:])
-            todos = get_todo("to-do.txt")
+            todos = functions.get_todo()
             index = num - 1
             todo_to_remove = todos[index].strip("\n")
             todos.pop(index)
 
-            with open("to-do.txt", "w") as file:
-                todos = file.writelines(todos)
+            functions.write_todo(todos)
             print(f"{todo_to_remove} was marked completed and has been removed from the to-do list")
         except IndexError:
             print("invalid index, please enter an index that exists")
